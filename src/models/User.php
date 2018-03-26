7149080ff009a0e0e3540dfe062b9b6d4944fb1d
Sean <sean@fruitstudios.co.uk>
2018-03-26 23:04:38 +0100

    Style tweaks, icon and screenshots

--- src/models/User.php
+++ src/models/User.php
@@ -33,7 +33,7 @@ class User extends ElementLink
         return 'linkit/types/settings/_user';
     }
 
-    public static function defualtUserPath(): string
+    public static function defaultUserPath(): string
     {
         return 'user/{id}';
     }
@@ -45,7 +45,7 @@ class User extends ElementLink
     {
         if($this->getUser())
         {
-            $userPath = static::defualtUserPath();
+            $userPath = static::defaultUserPath();
             if($this->userPath && $this->userPath != '')
             {
                 $userPath = $this->userPath;
