aaffdc7fb4970b46b80eee35d6c55b0832fa8a72
Sam Hibberd <samhibberd@me.com>
2021-10-07 23:47:14 +0100

    Migration

--- src/migrations/m201204_090026_pressed_transfer.php
+++ src/migrations/m201204_090026_pressed_transfer.php
@@ -20,7 +20,40 @@ class m201204_090026_pressed_transfer extends Migration
 
         if (version_compare($schemaVersion, '1.2.0', '<'))
         {
+            $fieldsService = Craft::$app->getFields();
+            foreach ($fieldsService->getAllFields() as $field)
+            {
+                if ($field instanceof \fruitstudios\linkit\fields\LinkitField)
+                {
+                    // Update the type settings
+                    $fieldSettings = $field->getSettings();
+                    $newTypes = [];
+                    foreach($fieldSettings['types'] as $typeClass => $typeSettings)
+                    {
+                        $newTypes[str_replace('fruitstudios', 'presseddigital', $typeClass)] = $typeSettings;
+                    }
+                    $fieldSettings['types'] = $newTypes;
 
+                    // Create field of new type
+                    $field = $fieldsService->createField([
+                        'type' => \fruitstudios\linkit\fields\LinkitField::class,
+                        'id' => $field->id,
+                        'uid' => $field->uid,
+                        'groupId' => $field->groupId,
+                        'name' => $field->name,
+                        'handle' => $field->handle,
+                        'columnSuffix' => $field->columnSuffix ?? null,
+                        'instructions' => $field->instructions,
+                        'searchable' => (bool)$field->searchable,
+                        'translationMethod' => $field->translationMethod,
+                        'translationKeyFormat' => $field->translationKeyFormat,
+                        'settings' => $fieldSettings,
+                    ]);
+
+                    // Save new field
+                    $fieldsService->saveField($field);
+                }
+            }
         }
     }
 
