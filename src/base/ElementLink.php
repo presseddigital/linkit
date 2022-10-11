65695c6be1acee74c968a2aae6a0527e43625023
Roel van Hintum <roel@born05.com>
2022-10-11 09:57:28 +0200

    Fix getUrl return on elementlink

--- src/base/ElementLink.php
+++ src/base/ElementLink.php
@@ -68,7 +68,7 @@ abstract class ElementLink extends Link
     public function getUrl(): string
     {
         $element = $this->getElement();
-        return $element ? $element->getUrl() : '';
+        return $element ? (string) $element->getUrl() : '';
     }
 
     public function getText(): string
